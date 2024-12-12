e1450af97d2638cfaf00996263a3db04c780460a
Matthis <113370391+mattbass93@users.noreply.github.com>
2024-12-12 19:22:48 +0100

    Antoine 1

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './assets/styles/global.scss';
 import Home from './pages/Home';
 import About from './pages/About'
@@ -9,15 +9,13 @@ import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/kasa/" element={<Home />} />
-        <Route path="/kasa/kasa/kasa/about" element={<About />} />
-        <Route path="/kasa/logement/:id" element={<Logement />} />
-        {/* Catch-all route for 404 */}
-        <Route path="/kasa/kasa/*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/kasa/" element={<Home />} />
+      <Route path="/kasa/kasa/about" element={<About />} />
+      <Route path="/kasa/logement/:id" element={<Logement />} />
+      {/* Catch-all route for 404 */}
+      <Route path="/kasa/kasa/*" element={<NotFound />} />
+    </Routes>
   );
 }
 
